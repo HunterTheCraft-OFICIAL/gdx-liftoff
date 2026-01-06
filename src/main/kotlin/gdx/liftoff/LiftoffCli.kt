234e6eeb76734d3ddf80caaaea2f42443a9a4b8b
HunterTheCraft-OFICIAL <impahunter67@gmail.com>
2026-01-06 13:54:34 -0300

    feat: git checkout liftoff-headless-cli-mode

--- src/main/kotlin/gdx/liftoff/LiftoffCli.kt
+++ src/main/kotlin/gdx/liftoff/LiftoffCli.kt
@@ -1,29 +1,7 @@
 package gdx.liftoff
 
 class LiftoffCli {
-
-  fun run(args: Array<String>) {
-    println(
-      """
-      Liftoff Headless CLI – Probe Mode
-
-      This is a diagnostic entrypoint.
-      No project generation is performed.
-      """.trimIndent(),
-    )
-
-    if (args.isEmpty()) {
-      println("No arguments provided.")
-      return
-    }
-
-    println("Arguments:")
-    args.forEachIndexed { index, arg ->
-      println("  [$index] $arg")
-    }
+  fun run() {
+    println("Liftoff CLI probe running")
   }
-}
-
-fun main(args: Array<String>) {
-  LiftoffCli().run(args)
 }
